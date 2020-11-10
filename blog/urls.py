7b0e725e7f70d69cfe704a4e3c10b95ebdfd7e9d
goplen-spec <nikita120805@mail.ru>
2020-11-11 00:01:49 +0500

    add static

--- blog/urls.py
+++ blog/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('movies/', admin.site.urls),
     path("", include('movies.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'movies.views.custom_page_not_found_view'
